801eff0f378360b1f3a81f629ef8dc7721bf63b9
Mratyunjay Tripathi <techspiriteagle@gmail.com>
2020-10-30 20:52:53 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,20 @@
 
 
 // export class using module.exports
+
+// import Shape from './shape';
+
+let  Shape=require('./shape');
+
+module.exports= class Circle extends Shape{
+    constructor(){
+        super();
+    }
+
+    calculateArea(){
+        console.log("Area of circle");
+
+        return 3.14*5*5;
+    }
+}
+
